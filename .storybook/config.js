852f2744b3a3819b3cd5b9e70ab630781bd77cea
José Luis de Vega <jose@redradix.com>
2017-10-13 13:29:46 +0200

    add storybook and enzyme for model check and testing

---
+++ .storybook/config.js
@@ -0,0 +1,17 @@
+import { configure, addDecorator } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withSmartKnobs } from 'storybook-addon-smart-knobs'
+
+import '../src/styles/styles.css'
+
+const req = require.context('../src/', true, /stories.js$/)
+
+addDecorator(withSmartKnobs)
+addDecorator(withKnobs)
+
+function loadStories() {
+  const files = req.keys()
+  files.sort().forEach(filename => req(filename))
+}
+
+configure(loadStories, module)
