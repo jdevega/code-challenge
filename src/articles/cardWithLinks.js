bd58c87926073409b2579dd23b1c641505e0591a
Jose Luis <joseluis.devega@gmail.com>
2017-10-16 22:09:23 +0200

    update routes

---
+++ src/articles/cardWithLinks.js
@@ -0,0 +1,11 @@
+import { withHandlers, compose } from 'recompose';
+import { withRouter } from 'react-router';
+
+export default compose(
+  withRouter,
+  withHandlers({
+    onEditClick: props => () => props.history.push(`/edit/${props.id}`),
+    onViewClick: props => () => props.history.push(`/${props.id}`),
+  }),
+)
+;
