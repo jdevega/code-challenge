cf954842e41081fb75008d6247b3d8b31f0c5a49
Jose Luis <joseluis.devega@gmail.com>
2017-10-16 22:10:31 +0200

    add SET_ONE action type

--- src/articles/actionTypes.js
+++ src/articles/actionTypes.js
@@ -1,4 +1,5 @@
 import { ACTIONS_PREFIX } from './constants';
 
-export const SET_ALL = `${ACTIONS_PREFIX}_SET_ALL`
-;
+export const SET_ALL = `${ACTIONS_PREFIX}_SET_ALL`;
+export const SET_ONE = `${ACTIONS_PREFIX}_SET_ONE`;
+
