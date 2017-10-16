8153305939e413c6f956fbc8c1d950df91de5064
Jose Luis <joseluis.devega@gmail.com>
2017-10-16 22:12:09 +0200

    fix selectors

--- src/articles/selectors.js
+++ src/articles/selectors.js
@@ -1,6 +1,6 @@
-import { propOr, prop } from 'ramda';
+import { propOr, pathOr } from 'ramda';
 import { DOMAIN } from './constants';
 
 const root = propOr({}, DOMAIN);
-export const getAll = state => propOr([], 'list')(root(state));
-export const getOne = state => prop('item', root(state));
+export const getAll = state => ({ articles: propOr([], 'list')(root(state)) });
+export const getOne = state => pathOr({}, ['item'], root(state));
