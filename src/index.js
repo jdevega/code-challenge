40e3240582ff96bffc8a08515f1658020d129e81
Jose Luis <joseluis.devega@gmail.com>
2017-10-17 23:13:06 +0200

    add new routes for create and edit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import { Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
@@ -10,17 +10,30 @@ import store, { history } from './store';
 import AppLayout from './ui/layout/App';
 import Articles from './ui/screens/Articles';
 import Article from './ui/screens/Article';
-import EditArticle from './ui/screens/EditArticle';
-import enhanceArticles from './articles/enhanceArticles';
-import enhanceArticle from './articles/enhanceArticle';
-import withHomeLink from './links/withHomeLink';
+import ArticleForm from './ui/screens/ArticleForm';
+import enhanceArticles from './articles/enhancers/enhanceArticles';
+import enhanceArticle from './articles/enhancers/enhanceArticle';
+import withForm from './forms/withForm';
+
+import { createArticle, editArticle } from './articles/actions';
 
 const EnhancedArticles = enhanceArticles(Articles);
 const EnhancedArticle = enhanceArticle(Article);
-const HomeLink = withHomeLink(Menu.Item);
 const menuItems = [
-  <HomeLink as="a" key="Articles">Articles</HomeLink>,
+  <Menu.Item key="Articles"><Link to="/" >Articles</Link></Menu.Item>,
+  <Menu.Item key="CreateArticle"><Link to="/create">New Article</Link></Menu.Item>,
 ];
+const CreateArticle = withForm({
+  title: 'Create Article',
+  actions: { createArticle },
+  onSubmit: (values, dispatch, props) => props.createArticle(values),
+})(ArticleForm);
+
+const EditArticle = withForm({
+  title: 'Edit Article',
+  actions: { editArticle },
+  onSubmit: (values, dispatch, props) => props.editArticle(values),
+})(ArticleForm);
 
 const router = (
   <Provider store={store}>
@@ -28,6 +41,7 @@ const router = (
       <AppLayout menuItems={menuItems}>
         <Route exact path="/" component={EnhancedArticles} />
         <Switch>
+          <Route exact path="/create" component={CreateArticle} />
           <Route exact path="/edit/:id" component={EditArticle} />
           <Route exact path="/:id" component={EnhancedArticle} />
         </Switch>
