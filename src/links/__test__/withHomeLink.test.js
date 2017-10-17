38d53c85fae12583bbeff504ad46b800cf0cb473
Jose Luis <joseluis.devega@gmail.com>
2017-10-17 10:08:05 +0200

    withHomeLink test

--- src/links/__test__/withHomeLink.test.js
+++ src/links/__test__/withHomeLink.test.js
@@ -1,4 +1,19 @@
-xdescribe('Links withHomeLink', () => {
-  xit('Check props on enhanced Component');
+import React from 'react';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router-dom';
+
+import withHomeLink from '../withHomeLink';
+
+describe('Links withHomeLink', () => {
+  const Mock = () => <div />;
+
+  it('Adds onClick to enhanced Component', () => {
+    const Enhanced = withHomeLink(Mock);
+    const wrapper = mount(<MemoryRouter><Enhanced /></MemoryRouter>);
+
+    const subject = wrapper.find(Mock).first();
+    expect(subject.exists()).toBe(true);
+    expect(subject.prop('onClick')).toBeDefined();
+  });
 })
 ;
